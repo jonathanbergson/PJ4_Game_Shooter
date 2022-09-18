4791cf90d9cb4a28e7b7d2869f96ecbfb7806b3d
Jonathan Bergson <contato@bergson.me>
2022-09-18 16:40:48 -0300

    chore: adicionado auto destroy no tiro

--- Assets/Scripts/Ship/Bullet/Bullet.cs
+++ Assets/Scripts/Ship/Bullet/Bullet.cs
@@ -6,6 +6,8 @@ namespace Ship.Bullet
     [RequireComponent(typeof(Rigidbody))]
     public class Bullet : MonoBehaviour
     {
+        private const float TimeToAutoDestroy = 10.0f;
+
         private void Awake()
         {
             Collider bulletCollider = GetComponent<Collider>();
@@ -14,15 +16,22 @@ namespace Ship.Bullet
             Rigidbody bulletRigidbody = GetComponent<Rigidbody>();
             bulletRigidbody.useGravity = false;
             bulletRigidbody.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionY;
+
+            Invoke(nameof(AutoDestroy), TimeToAutoDestroy);
         }
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.CompareTag(Constants.TagEnemy) == false) return;
 
-            Destroy(gameObject);
+            AutoDestroy();
             Destroy(other.gameObject);
             Manager.Instance.IncrementTotalPoints();
+        }
+
+        private void AutoDestroy()
+        {
+            Destroy(gameObject);
         }
     }
 }
